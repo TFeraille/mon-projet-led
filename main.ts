408845f24694b8d8833a7baac068e63fa23f801e
TFeraille <128042348+TFeraille@users.noreply.github.com>
2023-03-16 16:29:19 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,30 @@
+function Clignote_Droite () {
+    Droite.showColor(neopixel.colors(NeoPixelColors.Orange))
+    basic.pause(500)
+    Droite.showColor(neopixel.colors(NeoPixelColors.Black))
+    basic.pause(500)
+}
 input.onButtonPressed(Button.A, function () {
-    Gauche.showColor(neopixel.colors(NeoPixelColors.Blue))
+    for (let index = 0; index <= 2; index++) {
+        Clignote_Gauche()
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     Stop.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(1000)
+    Stop.showColor(neopixel.colors(NeoPixelColors.Black))
+    basic.pause(1000)
 })
+function Clignote_Gauche () {
+    Gauche.showColor(neopixel.colors(NeoPixelColors.Orange))
+    basic.pause(500)
+    Gauche.showColor(neopixel.colors(NeoPixelColors.Black))
+    basic.pause(500)
+}
 input.onButtonPressed(Button.B, function () {
-    Droite.showColor(neopixel.colors(NeoPixelColors.White))
+    for (let index = 0; index <= 2; index++) {
+        Clignote_Droite()
+    }
 })
 let Droite: neopixel.Strip = null
 let Stop: neopixel.Strip = null
@@ -14,7 +33,6 @@ let strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
 Gauche = strip.range(0, 2)
 Stop = strip.range(0, 5)
 Droite = strip.range(3, 2)
-strip.showRainbow(1, 360)
 basic.forever(function () {
 	
 })
